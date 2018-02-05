852b6ca51c99c35cb74fbad6b43f49c7d120a576
Lillian Chernin <l.smith1654@gmail.com>
2018-02-05 12:11:30 -0600

    update expanded browse menu

--- src/components/browseMenu/BrowseMenuExpansion.js
+++ src/components/browseMenu/BrowseMenuExpansion.js
@@ -7,9 +7,18 @@ class BrowseMenuExpansion extends Component {
     let genreList = ["Art","Business","Christian","Classics","Cookbooks","Fantasy","Fiction","History","Horror","Music","Nonfiction","Poetry","Science","Science Fiction","Self Help","Young Adult"];
     let formats = ["Book","Book w/CD","Book w/DVD","CD","DVD"];
     let renderedGenreList;
+    renderedGenreList = genreList.map((genre) => {
+      return (
+        <div>
+        <Link to={('/browse/' + genre)}>{genre}</Link>
+        <br />
+        </div>
+      )
+    })
     return (
       <div className="BrowseMenuExpansion">
         <h2>Genres</h2>
+        <div className="GenreList">{renderedGenreList}</div>
       </div>
     )
   }
